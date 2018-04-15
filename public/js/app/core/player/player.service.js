2892fd3f94a08d666af9377928e8c790176f231c
Felipe Marinho <felipevm97@gmail.com>
2018-04-15 20:33:48 -0300

    centralized listen function

--- public/js/app/core/player/player.service.js
+++ public/js/app/core/player/player.service.js
@@ -2,7 +2,7 @@
 
 var data = angular.module('player');
 
-data.factory('Player', function($resource, $window, $http) {
+data.factory('Player', function($resource, $window, $http, $log, Data, Lastfm2) {
   let videoID = '';
   let music = {};
   let queue = [];
@@ -35,6 +35,81 @@ data.factory('Player', function($resource, $window, $http) {
     return queue.pop();
   }
 
+  function getTimeout(music) {
+    return new Promise((resolve, reject) => {
+      let val =  Data.getYouTubeVideoDetails(_getVideoID()).then((response) => {
+        let duration = response.data.items[0].contentDetails.duration.replace('PT','');
+        let min = '';
+        let sec = '';
+        let flag = false;
+        for (var i = 0, n = duration.length; i < n; ++i) {
+          if (!flag && (duration[i] != 'M' && duration[i] != 'S')) {
+            min += duration[i];
+          } else if (flag && (duration[i] != 'M' && duration[i] != 'S')) {
+            sec += duration[i];
+          } else {
+            flag = true;
+          }
+        }
+
+        let minSum = parseInt(min);
+        let secSum = parseInt(sec);
+        return ((minSum * 60) + secSum)*1000;
+      });
+
+      val.then(resolve(val.then((r) => r)));
+
+    });
+  }
+
+  function isScrobbled() {
+    return music.scrobbled;
+  }
+
+  function setScrobbled(_scrobbled) {
+    music.scrobbled = _scrobbled;
+  }
+
+  let _listen = (music) => {
+    Data.setLastMusic(music);
+
+    Data.getYouTubeVideoID(music).then((response) => {
+      let videoID = response.data.items[0].id.videoId;
+      _setVideoID(videoID);
+      _setMusic(music);
+      Lastfm2.Track.sendNowPlaying(music.artist, music.name, music.album).then((response) => {
+        $log.info(`Lastfm2 Service: Now playing ${music.name} - ${music.artist}`);
+        setScrobbled(false);
+      });
+
+      $window.document.title = music.name + " - " + music.artist + " | Musicoteca";
+      let timestamp = (new Date().getTime())/1000;
+      getTimeout(music).then((response) => {
+        $window.setTimeout(() => {
+          if ((_getMusic().id == music.id)) {
+            $window.document.title = "Musicoteca";
+            if (_hasNext()) {
+              let next = _getNext();
+              _listen(next);
+            } else {
+              _setVideoID("");
+            }
+          }
+        }, response);
+        $window.setTimeout(() => {
+          if ((_getMusic().id == music.id) && !isScrobbled()) {
+            Lastfm2.Track.scrobble(music.artist, music.name, timestamp, music.album).then((response) => {
+              setScrobbled(true);
+              $log.info(`Lastfm2 Service: Scrobble "${music.name} - ${music.artist}" enviado.`);
+            });
+          }
+        }, response*0.7);
+      })
+
+    });
+
+  }
+
   return {
     setVideoID: _setVideoID,
     getVideoID: _getVideoID,
@@ -42,6 +117,7 @@ data.factory('Player', function($resource, $window, $http) {
     getMusic: _getMusic,
     addQueue: _addQueue,
     hasNext: _hasNext,
-    getNext: _getNext
+    getNext: _getNext,
+    listen: _listen
   }
 });
